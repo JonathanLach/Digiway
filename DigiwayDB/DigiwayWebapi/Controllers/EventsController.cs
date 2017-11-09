19b4d22d3f326a8e920b5dae796b231a3038322a
Astronauterz <orcalbinos@gmail.com>
2017-11-09 23:25:47 +0100

    Début WebAPI

---
+++ DigiwayDB/DigiwayWebapi/Controllers/EventsController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DigiwayModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigiwayWebapi.Controllers
+{
+    [Route("api/[controller]")]
+    public class EventsController : Controller
+    {
+        private DigiwayContext _context;
+
+        public EventsController(DigiwayContext context)
+        {
+            this._context = context;
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IEnumerable<Event>> Get()
+        {
+            return await _context.Events
+                    .Include(ec => ec.EventCategory)
+                    .Include(poi => poi.PointsOfInterest)
+                    .Include(c => c.Company)
+                    .ToListAsync();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IQueryable<Event>> Get(int id)
+        {
+            return await Task.FromResult(_context.Events.Where(e => e.EventId == id)
+                    .Include(ec => ec.EventCategory)
+                    .Include(poi => poi.PointsOfInterest)
+                    .Include(c => c.Company));
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- DigiwayDB/DigiwayWebapi/Startup.cs
+++ DigiwayDB/DigiwayWebapi/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using DigiwayModel;
+using Microsoft.EntityFrameworkCore;
 
 namespace DigiwayWebapi
 {
@@ -23,7 +25,9 @@ namespace DigiwayWebapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            var connection = @"Data Source = vm-sql2.iesn.Be\Stu3ig; Initial Catalog = 1718_etu31944_DB; User Id = 1718_etu31944; Password = ReuVA9^75sw";
+            services.AddMvc().AddJsonOptions(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+            services.AddDbContext<DigiwayContext>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
